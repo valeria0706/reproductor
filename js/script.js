aec90cdb696e3b230925c658019102cdb3825202
zMars5 <rmiguelucho@gmail.com>
2023-11-14 02:50:21 -0500

    Se agrega funcion pausar y imagenes giratorias

--- js/script.js
+++ js/script.js
@@ -1,16 +1,17 @@
 const wrapper = document.querySelector(".wrapper"),
-    musicImg = wrapper.querySelector(".img"),
+    musicImg = wrapper.querySelector(".img-area img"), 
     musicName = wrapper.querySelector(".name"),
     musicArtist = wrapper.querySelector(".artist"),
     playPauseBtn = wrapper.querySelector(".play-pause"),
     prevBtn = wrapper.querySelector("#prev"),
     nextBtn = wrapper.querySelector("#next"),
     mainAudio = wrapper.querySelector("#main-audio"),
-    progressArea = wrapper.querySelector(".progress-area"), 
+    progressArea = wrapper.querySelector(".progress-area"),
     progressBar = wrapper.querySelector(".progress-bar");
 
-    let musicIndex = Math.floor((Math.random()* allMusic.length)+1);
-    isMusicPaused = true;
+let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+let isMusicPaused = true;
+let rotationPaused = false; 
 
 window.addEventListener("load", () => {
     loadMusic(musicIndex);
@@ -19,15 +20,21 @@ window.addEventListener("load", () => {
 function loadMusic(indexNumb) {
     musicName.innerText = allMusic[indexNumb - 1].name;
     musicArtist.innerText = allMusic[indexNumb - 1].artist;
-    //musicImg.src=`./imges/${allMusic[indexNumb - 1].img}.jpg`;
-    mainAudio.src = `./music/${allMusic[indexNumb -  1].src}.mp3`;
+    musicImg.src = `./imges/${allMusic[indexNumb - 1].img}.jpg`;
+    mainAudio.src = `./music/${allMusic[indexNumb - 1].src}.mp3`;
+
+    if (!isMusicPaused) {
+        playMusic();
+    }
 }
 
 function playMusic() {
     wrapper.classList.add("paused");
-    //musicImg.classList.add('rotate');
+    musicImg.classList.add('rotate');
     playPauseBtn.innerHTML = `<i class="fi fi-sr-pause"></i>`;
     mainAudio.play();
+    isMusicPaused = false;
+    rotationPaused = false; 
 }
 
 function pauseMusic() {
@@ -35,16 +42,18 @@ function pauseMusic() {
     musicImg.classList.remove('rotate');
     playPauseBtn.innerHTML = `<i class="fi fi-sr-play"></i>`;
     mainAudio.pause();
+    isMusicPaused = true;
+    rotationPaused = true;
 }
 
-function prevMusic(){
+function prevMusic() {
     musicIndex--;
     musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex;
     loadMusic(musicIndex);
     playMusic();
 }
 
-function nextMusic(){
+function nextMusic() {
     musicIndex++;
     musicIndex > allMusic.length ? musicIndex = 1 : musicIndex = musicIndex;
     loadMusic(musicIndex);
@@ -58,53 +67,61 @@ playPauseBtn.addEventListener("click", () => {
 
 prevBtn.addEventListener("click", () => {
     prevMusic();
-} );
+});
 
 nextBtn.addEventListener("click", () => {
     nextMusic();
-} );
+});
 
-mainAudio.addEventListener("timeupdate", (e) =>{
+mainAudio.addEventListener("loadedmetadata", () => {
+    let mainAdDuration = mainAudio.duration;
+    if (!isNaN(mainAdDuration) && mainAdDuration > 0) {
+        updateDuration();
+    }
+});
+
+mainAudio.addEventListener("timeupdate", (e) => {
     const currentTime = e.target.currentTime;
     const duration = e.target.duration;
-    let progressWidth = (currentTime / duration)*100;
-    progressBar.style.width=`${progressWidth}%`;
+    let progressWidth = (currentTime / duration) * 100;
+    progressBar.style.width = `${progressWidth}%`;
 
-    let musicCurrentTime = wrapper.querySelector(".current-time"),
-    musicDuration = wrapper.querySelector(".max-duration");
+    let musicCurrentTime = wrapper.querySelector(".current-time");
 
-    mainAudio.addEventListener("loadeddata", () => {
-        let mainAdDuration = mainAudio.duration;
+    let currentMin = Math.floor(currentTime / 60);
+    let currentSec = Math.floor(currentTime % 60);
+    if (currentSec < 10) {
+        currentSec = `0${currentSec}`;
+    }
+    musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
+});
+
+function updateDuration() {
+    let mainAdDuration = mainAudio.duration;
+    if (!isNaN(mainAdDuration) && mainAdDuration > 0) {
+        let musicDuration = wrapper.querySelector(".max-duration");
         let totalMin = Math.floor(mainAdDuration / 60);
         let totalSec = Math.floor(mainAdDuration % 60);
-
-        if(totalSec < 10){
+        if (totalSec < 10) {
             totalSec = `0${totalSec}`;
         }
         musicDuration.innerText = `${totalMin}:${totalSec}`;
-
-    });
-    let currentMin = Math.floor(currentTime / 60);
-    let currentSec = Math.floor(currentTime % 60);
-    if(currentSec < 10){
-        currentSec = `0${currentSec}`;
-        musicCurrentTime.innerText = `${currentMin}:${currentSec}`;
     }
-});
+}
 
 progressArea.addEventListener("click", (e) => {
     let progressWidth = progressArea.clientWidth;
     let ClickedOffsetX = e.offsetX;
     let songDuration = mainAudio.duration;
 
-
-    mainAudio.currentTime = (ClickedOffsetX / progressWidth)* songDuration;
+    mainAudio.currentTime = (ClickedOffsetX / progressWidth) * songDuration;
     playMusic();
 });
 
-mainAudio.addEventListener("ended",() =>{
+mainAudio.addEventListener("ended", () => {
     nextMusic();
 });
+
 
 
 
