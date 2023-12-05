843d54ec12982dc0fb2c7bd927c7a6bab1b981c6
StivenRios26 <destlor2612@gmail.com>
2023-12-04 22:46:22 -0500

    dic 04

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -1,46 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "../../components/sidebar";
-import { setClientToken } from "../../../src/spotify";
-import Login from "../auth/login";
 import Favorites from "../favorites/index";
 import Feed from "../feed/index";
 import Library from "../library/index";
 import Player from "../player/index";
 import Trending from "../trending/index";
 import "./home.css";
+import Login from "../auth/login";
 
 export default function Home() {
-  const [token, setToken] = useState("");
-
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    const hash = window.location.hash;
-    window.location.hash = "";
-    if (!token && hash) {
-      const _token = hash.split("&")[0].split("=")[1];
-      window.localStorage.setItem("token", _token);
-      setToken(_token);
-      setClientToken(_token);
-    } else {
-      setToken(token);
-      setClientToken(token);
-    }
-  }, []);
-
-  return !token ? (
-    <Login />
-  ) : (
+  return(
     <Router>
       <div className="main-body">
-        <Sidebar />
+        <Login/>
+        {/* <Sidebar />
         <Routes>
           <Route path="/" element={<Library />} />
           <Route path="/feed" element={<Feed />} />
           <Route path="/trending" element={<Trending />} />
           <Route path="/player" element={<Player />} />
           <Route path="/favorites" element={<Favorites />} />
-        </Routes>
+        </Routes> */}
       </div>
     </Router>
   );
--- src/spotify.jsx
+++ src/spotify.jsx
@@ -2,13 +2,12 @@ import axios from "axios";
 
 
 const authEndpoint ="https://accounts.spotify.com/authorize?" ;
-const clientId="9d1c7fdee2eb4cf0a12e8d760f3e6b48";
-const redirectUri="http://localhost:3000";
+const clientId="28025523caaa4414b75a7b19baf732f0";
+const redirectUri="http://localhost:3000/";
 const scopes=["user-library-read, playlist-read-private"]; 
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-    "%20"
-  )}&response_type=token&show_dialog=true`;
+    "%20")}&response_type=token&show_dialog=true`;
   
   const apiClient = axios.create({
     baseURL: "https://api.spotify.com/v1/",
